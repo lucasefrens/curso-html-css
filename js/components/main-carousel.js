d537caee48185c2a3d1845e527413d10c31503a6
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-05-05 11:11:22 -0300

    Create script for main-carousel component

---
+++ js/components/main-carousel.js
@@ -0,0 +1,26 @@
+const btnLeft = window.document.querySelector(".button-arrow.-left");
+const btnRight = window.document.querySelector(".button-arrow.-right");
+const elements = window.document.querySelector(".elements");
+let pixels = 0;
+
+btnRight.addEventListener("click", function() {
+
+    pixels += 300;
+
+    elements.style = `transform: translateX(${pixels}px)`;
+
+    if (pixels >= 1500) {
+        pixels = -300;
+    }
+});
+
+btnLeft.addEventListener("click", function() {
+
+    pixels -= 300;
+
+    elements.style = `transform: translateX(${pixels}px)`;
+
+    if (pixels <= -1500) {
+        pixels = 300;
+    }
+});
